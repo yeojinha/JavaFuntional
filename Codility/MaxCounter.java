ee206809c152d97af06a730e2bfd5c2e19604358
Yeojin <hayeojin4966@gmail.com>
2020-08-31 21:12:58 +0900

    Update for문이 안에서 2번 돌면 77% 다 돌고 한 번만 돌면 o(n)이니 100%

--- Codility/MaxCounter.java
+++ Codility/MaxCounter.java
@@ -8,8 +8,19 @@ public class MaxCounter {
 		for(int i:result) {
 			System.out.print(i+"");
 		}
+		System.out.println();
+		int[]result_1=solution_1(5,arr);
+		for(int i:result_1) {
+			System.out.print(i+"");
+		}
+		System.out.println();
+		int []result_2=solution_2(5,arr);
+		for(int i:result_2) {
+			System.out.print(i+"");
+		}
 	}
-	public static int[] solution(int N, int[] A) {
+	public static int[] solution(int N, int[] A) {//77%
+		//譆學曖 唳辦 O(N^2);
 		int[] result = new int[N];
 		for (int i = 0; i < A.length; i++) {
 			int operation = A[i];
@@ -35,5 +46,51 @@ public class MaxCounter {
 			A[i] = max;
 		}
 	}
+	private static int [] solution_1(int N, int[] A) {//77%
+		//譆學曖 唳辦 O(N^2);
+		int max=0;
+		//int []arr= {3,4,4,6,1,4,4};
+		int []mty=new int [N];
+		for(int i:A) {
+			if(i<=N) {
+				mty[i-1]++;	
+				max=mty[i-1];
+			}
+			if(i==N+1) {
+				for(int j=0;j<mty.length;j++) {
+					mty[j]=max;
+				}
+			}
+		}
+		return mty;
+	}
+	
+	
+	private static int [] solution_2(int N, int [] A) {//100%
+		
+		 int length = A.length, maxOfCounter = 0, lastUpdate = 0;
+	      int applyMax = N + 1;
+	      int result[] = new int[N];
 
+	      for (int i = 0; i < length; ++i ) {
+	          if(A[i] == applyMax){
+	              lastUpdate = maxOfCounter;
+	          } else if (A[i] <= N)  {
+	              int position = A[i]-1;
+	              result[position] = result[position] > lastUpdate
+	                                        ? result[position] + 1 : lastUpdate + 1;
+	              // updating the max for future use
+	              if(maxOfCounter <=  result[position]) {
+	                  maxOfCounter = result[position];
+	              }
+	          }
+	     }
+	     // updating all the values that are less than the lastUpdate to the max value
+	     for (int i = 0; i < N; ++i) {
+	         if(result[i] < lastUpdate) {
+	             result[i] = lastUpdate;
+	         }
+	     }
+	     return result;
+	}
 }
